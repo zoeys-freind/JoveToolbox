dc0d794949fe9ab26e558cf26611851f0cd8f18a
zoeys-freind <129983142+zoeys-freind@users.noreply.github.com>
2024-01-24 20:44:08 -0600

    Update __init__.py

--- jovetools/jovetools/shell/__init__.py
+++ jovetools/jovetools/shell/__init__.py
@@ -1,5 +1,15 @@
-import sys
+import sys, os
 # command: jtools <command> <args>
+command_dict = {}
+# command creation decorator
+# this makes it really simple to make the commands
+def mk_command(name:str, desc:str="No description provided.", perams:tuple=()):
+    def decor(func):
+        command_dict[name] = {"func": func, "desc": desc, "perams": perams}
+    return decor
+
+thisdir = os.path.dirname(os.path.abspath(__file__))
+
 def main():
     command = ""
     cmd_args = []
@@ -23,14 +33,6 @@ def main():
 ╰────────────────────────╯
 """[1:-1] # recommended to have MENUNM at 10 characters always
 
-    command_dict = {}
-    # command creation decorator
-    # this makes it really simple to make the commands
-    def mk_command(name:str, desc:str="No description provided.", perams:tuple=()):
-        def decor(func):
-            command_dict[name] = {"func": func, "desc": desc, "perams": perams}
-        return decor
-
     # perams beginning with '*' are required.
 
     # - command definitions - #
@@ -44,10 +46,23 @@ def main():
         cnfg = menu.replace(":[MENUNM]:", "  Config  ")
         settings = []
 
+    try:
+        global cma
+        cma = "<importloop>"
+        for cmm in os.listdir(f"{os.path.dirname(thisdir)}/submodules"):
+            if not cmm.startswith("__"):
+                cma = cmm
+                with open(f"{os.path.dirname(thisdir)}/submodules/{cmm}", "r") as fm:
+                    cmdict = {"mk_command": mk_command, "command_dict": command_dict, "__file__": f"{os.path.dirname(thisdir)}/submodules/{cmm}"}
+                    exec(fm.read(), cmdict)
+                    globals()["command_dict"] = cmdict["command_dict"]
+    except Exception as e:
+        print(f"Could not finish loading submodules!\n('{cma}' raised a {e.__class__.__name__})")
+
     usg = ""
 
     for cmd, info in command_dict.items():
-        
+
         usg += "\n├┤ " + cmd + " ↴"
         usg += "\n│┃ '" + info["desc"] + "'"
         argindx = 0
@@ -57,10 +72,10 @@ def main():
             usg += f"\n││{argindx}┃ '" + peram[1] + "'"
             usg += "\n│├─╯"
         usg += "\n├┘"
-        
+
 
     usage = menu.replace(":[LISTING]:", usg).replace(":[MENUNM]:", "Usage Tree")
-    
+
     # - command execution - #
     if command in command_dict:
         command_dict[command]["func"](*cmd_args)
